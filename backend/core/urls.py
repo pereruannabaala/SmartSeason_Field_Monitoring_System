7af8cca2e7de536fe2730ffc2816bc82752c77f5
pereruannabaala <pereruannabaala@gmail.com>
2026-04-23 15:54:55 +0300

    push changes

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
 from fields.views import FieldViewSet, MyTokenObtainPairView, agent_list
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'fields', FieldViewSet, basename='field')
@@ -14,3 +16,5 @@ urlpatterns = [
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/users/', agent_list),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
