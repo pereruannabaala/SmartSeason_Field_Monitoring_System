93959756ca3e3bdd9846ab3d4ed33defb3587592
pereruannabaala <pereruannabaala@gmail.com>
2026-04-23 22:36:22 +0300

    Switch to psycopg2-binary for Vercel compatibility

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
